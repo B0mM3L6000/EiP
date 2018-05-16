0768fbd353694cfd4fcbdc835e773f310d23e177
Martin <b0mm3l@gmail.com>
2018-05-16 11:43:55 +0200

    uebung 14.1

--- Uebung5/Uebung5_Aufgabe14_1.py
+++ Uebung5/Uebung5_Aufgabe14_1.py
@@ -16,12 +16,42 @@ found = False
 
 #CODE:
 
+#länge des felds:
 
 
+def suche(x, feld):
+    gefunden = False
+    #Punkt zum testen:
+    n = len(feld)
+    #print("Längefeld:",n)
+    if n%2 == 0:
+        n = n//2
+    else:
+        n = (n+1)//2
+    #print(n)
+    #print("elementwert:", feld[n-1])
+    #testen ob stelle n-1 (wegen index 0 bis len(feld)-1) die gesuchte zahl ist:
+    if feld[n-1] == x:
+        gefunden = True
+        #print("gefunden")
+    #rekursiv mit neuem feld je nachdem ob x kleiner oder größer feld[n-1]
+    elif feld[n-1] > x:
+        feld = feld[0:n]
+        #print("länge neues feld:", len(feld))
+        #print("x ist kleiner",feld)
+        gefunden = suche(x,feld)
+    elif feld[n-1] < x:
+        feld = feld[n:len(feld)]
+        #print("länge neues feld:", len(feld))
+        #print("x ist größer", feld)
+        gefunden = suche(x, feld)
+    return gefunden
 
+#ausführen:
 
-
-
+found = suche(x, a)
+#print(a)
+#print(found)
 
 
 #sauce footer:
